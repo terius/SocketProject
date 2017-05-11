3221473e7acc2d0348489209bb7e479c8b01f310
teriusstar <18728856@qq.com>
2017-05-11 10:09:20 +0800

    添加项目文件。

---
+++ SocketProject/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SocketProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+
+
+
+        #region Task Test
+        static void TestTask()
+        {
+            string c = "";
+            Action<string, string> a1 = (aa, bb) => { c = aa + bb; };
+            Action a2 = () => { c = "CCCCCCC"; };
+            var t = Task.Run(async () =>
+            {
+                return await GetNowTime();
+            });
+
+            Console.WriteLine("start ThreadId:" + Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine(t.Result);
+            Console.WriteLine("end ThreadId:" + Thread.CurrentThread.ManagedThreadId);
+        }
+        static async Task<string> GetNowTime()
+        {
+            Thread.Sleep(1000);
+            var t = await Task.FromResult(DateTime.Now.ToLongTimeString() + "   ThreadId:" + Thread.CurrentThread.ManagedThreadId);
+            return t;
+        }
+
+        #endregion
+
+
+
+    }
+}
